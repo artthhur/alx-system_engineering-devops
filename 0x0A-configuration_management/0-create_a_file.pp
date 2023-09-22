3f365e2c85967c5d38017484a38d04f6a1c0146a
samad <abdessamad.bouchelga@gmail.com>
2023-09-22 15:32:03 -0700

    tasks

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# create a file in /tmp
+file { 'school':
+  ensure  => 'present',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+  path    => '/tmp/school',
+}
