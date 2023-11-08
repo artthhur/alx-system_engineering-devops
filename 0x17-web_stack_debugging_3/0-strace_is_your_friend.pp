91cd89c06f85da3728d14fd32cf587fc8f628ad8
artthhur <abdessamad.bouchelga@gmail.com>
2023-11-08 14:09:55 -0800

    tasks

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# change extention from .phpp to .php
+# file: /var/www/html/wp-settings.php
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
